77ff4a6f024d767335eb96ab834dcfd46c937c35
awaisspk <iamawaisiqball@gmail.com>
2022-01-23 19:59:16 +0500

    reroute to login on initial load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import type {NextPage} from 'next';
+import {useRouter} from 'next/router';
+import {useLayoutEffect} from 'react';
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useLayoutEffect(() => {
+    router.push('/login');
+  });
   return <div></div>;
 };
 
