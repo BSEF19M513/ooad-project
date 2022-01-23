0bd8d83bbc5130a62a8c04709ba0d20948c411c1
abdullah <bsef19m506@pucit.edu.pk>
2022-01-23 14:25:59 +0500

    signup and login and layoutchanges

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css';
-import type {AppProps} from 'next/app';
-import {NextUIProvider} from '@nextui-org/react';
-import {AppLayout} from '../src/Layouts/AppLayout';
+import type { AppProps } from 'next/app';
+import { NextUIProvider } from '@nextui-org/react';
+import { AppLayout } from '../src/Layouts/AppLayout';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp ({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
       <AppLayout>
-        <Component {...pageProps} />
+        <div className='container heightContainer'>
+          <Component { ...pageProps } />
+        </div>
       </AppLayout>
     </NextUIProvider>
   );
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -9,25 +9,26 @@ type NavProps = {
   links: Link[];
 };
 
-export const Nav = ({links}: NavProps) => {
+export const Nav = ({ links }: NavProps) => {
   return (
     <div>
       <ul
-        style={{
+        style={ {
           display: 'flex',
           gap: '20px',
           color: '#0070f3',
-        }}
+          marginTop: '12px',
+        } }
       >
-        {links.map((link, i) => {
+        { links.map((link, i) => {
           return (
-            <li key={link.name + i}>
-              <Link href={link.url}>
-                <a>{link.name}</a>
+            <li key={ link.name + i }>
+              <Link href={ link.url }>
+                <a className='text-decoration-none btn btn-dark text-light'>{ link.name }</a>
               </Link>
             </li>
           );
-        })}
+        }) }
       </ul>
     </div>
   );
