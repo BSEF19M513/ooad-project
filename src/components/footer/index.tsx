c42d8a4a932919130bd2e180e27903fa0c95c45e
abdullah <bsef19m506@pucit.edu.pk>
2022-01-23 15:02:01 +0500

    link added

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import styles from '../../../styles/footer.module.css';
+import Link from "next/link";
 
 export const Footer = () => {
   return (<div className="footer bg-dark">
     <div className="d-flex justify-content-around align-items-center pt-4 pb-4">
       <div className="text-light">
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
       </div>
       <div className="text-light">
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
       </div>
       <div className="text-light">
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
-        <p className="h6"><a className="btn btn-dark text-light" href="">Lorem ipsum dolor</a> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
+        <p className="h6"><Link href=""><a className="btn btn-dark text-light">Lorem ipsum dolor</a></Link> </p>
       </div>
     </div>
     <div className="text-center text-muted h6 mb-0 pb-4">
